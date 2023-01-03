ce72dc7e03005d57fec970aa0e14fc2fc76de208
Ermakov Alexandr <ermakov.sascha2015yandex.ru@gmail.com>
2023-01-03 18:33:43 +0300

    Added "Likes" class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import Strings.Likes;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(Likes.whoLikesIt());
+        System.out.println(Likes.whoLikesIt("Peter"));
+        System.out.println(Likes.whoLikesIt("Jacob", "Alex"));
+        System.out.println(Likes.whoLikesIt("Alex", "Jacob", "Mark"));
+        System.out.println(Likes.whoLikesIt("Alex", "Jacob", "Mark", "Max"));
     }
 }
